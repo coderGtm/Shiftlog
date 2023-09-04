ee350fa790a66d8082b1e95a6910bedc5e175a39
coderGtm <gautammehta0000@gmail.com>
2023-09-04 23:10:31 +0530

    shifted routing logic to seperate file

--- main.go
+++ main.go
@@ -5,34 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	// route urls
-	//
-	// Auth
-	router.POST("createAccount", createAccount)
-	router.DELETE("deleteAccount", deleteAccount)
-	router.POST("updateAccount", updateAccount)
-	router.GET("login", login)
-	router.GET("logout", logout)
-
-	// Dashboard
-	router.GET("getApps", getApps)
-	router.POST("createApp", createApp)
-	router.DELETE("deleteApp", deleteApp)
-	router.POST("hideApp", hideApp)
-
-	// App
-	router.GET("getReleases", getReleases)
-	router.POST("createRelease", createRelease)
-	router.DELETE("deleteRelease", deleteRelease)
-	router.POST("hideRelease", hideRelease)
-
-	// Release
-	router.GET("getReleaseNotesTXT",getReleaseNotesTXT)
-	router.GET("getReleaseNotesMD", getReleaseNotesMD)
-	router.GET("getReleaseNotesHTML",getReleaseNotesHTML)
-	router.POST("updateReleaseNotesTXT",updateReleaseNotesTXT)
-	router.POST("updateReleaseNotesMD",updateReleaseNotesMD)
-	router.POST("updateReleaseNotesHTML",updateReleaseNotesHTML)
+	setUpRoutes(router)
 
 	router.Run(":8000")
 }
---
+++ routes.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func setUpRoutes(router *gin.Engine) {
+	// Auth
+	router.POST("createAccount", createAccount)
+	router.DELETE("deleteAccount", deleteAccount)
+	router.POST("updateAccount", updateAccount)
+	router.GET("login", login)
+	router.GET("logout", logout)
+
+	// Dashboard
+	router.GET("getApps", getApps)
+	router.POST("createApp", createApp)
+	router.DELETE("deleteApp", deleteApp)
+	router.POST("hideApp", hideApp)
+
+	// App
+	router.GET("getReleases", getReleases)
+	router.POST("createRelease", createRelease)
+	router.DELETE("deleteRelease", deleteRelease)
+	router.POST("hideRelease", hideRelease)
+
+	// Release
+	router.GET("getReleaseNotesTXT",getReleaseNotesTXT)
+	router.GET("getReleaseNotesMD", getReleaseNotesMD)
+	router.GET("getReleaseNotesHTML",getReleaseNotesHTML)
+	router.POST("updateReleaseNotesTXT",updateReleaseNotesTXT)
+	router.POST("updateReleaseNotesMD",updateReleaseNotesMD)
+	router.POST("updateReleaseNotesHTML",updateReleaseNotesHTML)
+}
