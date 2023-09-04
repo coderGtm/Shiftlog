e6b152b0dfcd4e4dd12f3d35d970a730fb196426
coderGtm <gautammehta0000@gmail.com>
2023-09-04 13:33:47 +0530

    created and routed empty global functions

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ func main() {
 	router.GET("login", login)
 	router.GET("logout", logout)
 
+	// Global
+	router.GET("getApps", getApps)
+	router.POST("createApp", createApp)
+	router.DELETE("deleteApp", deleteApp)
+	router.POST("hideApp", hideApp)
+
 	router.Run("localhost:8000")
 }
 
@@ -23,3 +29,9 @@ func deleteAccount(c *gin.Context) {}
 func updateAccount(c *gin.Context) {}
 func login(c *gin.Context) {}
 func logout (c *gin.Context) {}
+
+// Global
+func getApps(c *gin.Context) {}
+func createApp(c *gin.Context) {}
+func deleteApp(c *gin.Context) {}
+func hideApp(c *gin.Context) {}
