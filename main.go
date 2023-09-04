3b65f37938aabd4706c569a9c8407e556422ea01
coderGtm <gautammehta0000@gmail.com>
2023-09-04 13:23:11 +0530

    created and routed empty auth functions

--- main.go
+++ main.go
@@ -5,5 +5,21 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
+	// route urls
+	//
+	// Auth
+	router.POST("createAccount", createAccount)
+	router.POST("deleteAccount", deleteAccount)
+	router.POST("updateAccount", updateAccount)
+	router.GET("login", login)
+	router.GET("logout", logout)
+
 	router.Run("localhost:8000")
 }
+
+// Auth
+func createAccount(c *gin.Context) {}
+func deleteAccount(c *gin.Context) {}
+func updateAccount(c *gin.Context) {}
+func login(c *gin.Context) {}
+func logout (c *gin.Context) {}
