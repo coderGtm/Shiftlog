ffb6641456ac8671a273d863cea962d13e8c741d
coderGtm <gautammehta0000@gmail.com>
2023-09-10 23:10:45 +0530

    decapitalized an error string

--- authUtils.go
+++ authUtils.go
@@ -115,7 +115,7 @@ func extractAuthToken(c *gin.Context) string {
 func extractUserIdFromToken(tokenString string) (uint, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
 	})
