6de8723a1a29ff93da3b26dab7b0f21d8eca43d4
coderGtm <gautammehta0000@gmail.com>
2023-09-18 14:37:18 +0530

    merge bug fix

--- routes.go
+++ routes.go
@@ -11,18 +11,8 @@ func setUpRoutes(router *gin.Engine) {
 	api.PUT("updatePassword", updatePassword)
 	api.POST("login", login)
 	api.GET("logout", logout)
-	api.POST("createAccount", createAccount)
-	api.DELETE("deleteAccount", deleteAccount)
-	api.PUT("updateUsername", updateUsername)
-	api.PUT("updatePassword", updatePassword)
-	api.POST("login", login)
-	api.GET("logout", logout)
 
 	// Dashboard
-	api.GET("getApps", getApps)
-	api.POST("createApp", createApp)
-	api.DELETE("deleteApp", deleteApp)
-	api.PUT("updateApp", updateApp)
 	api.GET("getApps", getApps)
 	api.POST("createApp", createApp)
 	api.DELETE("deleteApp", deleteApp)
