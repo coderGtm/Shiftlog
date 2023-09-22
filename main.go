3ea068ca09807415da929ded2308bf4b5fd2d568
coderGtm <gautammehta0000@gmail.com>
2023-09-22 12:59:33 +0530

    added a port environment variable

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/microcosm-cc/bluemonday"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/microcosm-cc/bluemonday"
 )
 var db *sql.DB
 var dbErr error
@@ -29,5 +30,5 @@ func main() {
 
 	setUpRoutes(router)
 
-	router.Run(":8080")
+	router.Run(":" + os.Getenv("PORT"))
 }
